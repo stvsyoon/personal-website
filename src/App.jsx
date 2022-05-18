e1c84da6a8c63bc656f4e1c8fb7673b021b0696e
Steve Yoon <steve@Steves-MacBook-Pro.local>
2022-05-17 21:16:19 -0700

    added components, background, and styles in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Education from './components/education/Education'
+import Photography from './components/photography/Photography'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+      <>
+        <Header />
+        <Navbar />
+        <About />
+        <Experience />
+        <Education />
+        <Photography />
+        <Contact />
+        <Footer />
+      </>
   )
 }
 
---
+++ src/components/photography/Photography.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './photography.css'
+
+const Photography = () => {
+  return (
+    <div>Photography</div>
+  )
+}
+
+export default Photography
