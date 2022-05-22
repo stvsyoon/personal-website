25cae5f460aed42169afb35fe3076948b8d7d97d
Steve Yoon <steve@Steves-MacBook-Pro.local>
2022-05-22 15:51:59 -0700

    Completed responsive header section

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,14 @@
+// call to action component
+import React from 'react'
+import Resume from "../../assets/resume2.pdf"
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href={Resume} download className='btn'>Download Resume</a>
+        <a href="#contact" className='btn btn-primary'>Contact Me</a>
+    </div>
+  )
+}
+
+export default CTA
